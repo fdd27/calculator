c543021d6e4079cedba4222e2e7d7c690258b642
fdd <fddfddfddfddfddfdd@gmail.com>
2021-05-24 07:44:01 +0300

    Basic operator functions

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function add(a, b) {
+    return a + b;
+}
 
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(op, num1, num2) {
+    if (op == "+") add(num1, num2);
+    if (op == "-") subtract(num1, num2);
+    if (op == "*") multiply(num1, num2);
+    if (op == "/") divide(num1, num2);
+}
