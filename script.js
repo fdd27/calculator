9d9d53ef9417fb2e4d3f5004eebeb49417e6d7c2
fdd <fddfddfddfddfddfdd@gmail.com>
2021-05-24 08:15:13 +0300

    HTML basis & screen functionality

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const btnNumbers = document.querySelectorAll('[data-number]');
+const btnOperators = document.querySelectorAll('[data-operator]');
+const btnEquals = document.querySelector('[data-equals]');
+const btnUndo = document.querySelector('[data-undo]');
+const btnClear = document.querySelector('[data-clear]');
+
+let screen = document.querySelector('[data-screen]');
+let reset = false;
+
+btnNumbers.forEach((btnNumber) => {
+    btnNumber.addEventListener('click', () => "");
+})
+
 function add(a, b) {
     return a + b;
 }
@@ -19,4 +32,14 @@ function operate(op, num1, num2) {
     if (op == "-") subtract(num1, num2);
     if (op == "*") multiply(num1, num2);
     if (op == "/") divide(num1, num2);
+}
+
+function append(num) {
+    if (screen.textContent == "0" || reset) screen.textContent = "";
+    screen.textContent += num;
+}
+
+function resetScreen() {
+    screen.textContent = "";
+    reset = false;
 }
