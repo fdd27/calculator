f03761dbfdd6a201b4e37174ec59eee90a89d06b
fdd <fddfddfddfddfddfdd@gmail.com>
2021-05-28 16:59:01 +0300

    Basic functionality with only two numbers and one operator

--- script.js
+++ script.js
@@ -5,41 +5,78 @@ const btnUndo = document.querySelector('[data-undo]');
 const btnClear = document.querySelector('[data-clear]');
 
 let screen = document.querySelector('[data-screen]');
-let reset = false;
+//let reset = false;
+let num1 = null;
+let num2 = null;
+let operator = null;
+//let screenValue;
+
 
 btnNumbers.forEach((btnNumber) => {
-    btnNumber.addEventListener('click', () => "");
-})
+    btnNumber.addEventListener('click', () => append(btnNumber.textContent));
+});
+
+btnOperators.forEach((btnOperator) => {
+    btnOperator.addEventListener('click', () => {
+        operator = btnOperator.textContent;
+    });
+});
+
+btnEquals.addEventListener('click', () => {
+    if (num2 == null) screen.textContent = num1;
+    else operate(operator, num1, num2);
+    num1 = null;
+    num2 = null;
+    operator = null;
+});
+
+btnClear.addEventListener('click', reset);
 
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a, b) {
-    return a - b;
+    return Number(a) - Number(b);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return Number(a) * Number(b);
 }
 
 function divide(a, b) {
-    return a / b;
+    return Number(a) / Number(b);
 }
 
 function operate(op, num1, num2) {
-    if (op == "+") add(num1, num2);
-    if (op == "-") subtract(num1, num2);
-    if (op == "*") multiply(num1, num2);
-    if (op == "/") divide(num1, num2);
+    resetScreen();
+    if (op == "+") screen.textContent = add(num1, num2);
+    if (op == "-") screen.textContent = subtract(num1, num2);
+    if (op == "*") screen.textContent = multiply(num1, num2);
+    if (op == "/") screen.textContent = divide(num1, num2);
 }
 
 function append(num) {
-    if (screen.textContent == "0" || reset) screen.textContent = "";
-    screen.textContent += num;
+    //if (screen.textContent == "0" || reset) screen.textContent = "";
+    resetScreen();
+    if (num1 == null){
+        num1 = num;
+        screen.textContent += num;
+    } else {
+        num2 = num;
+        screen.textContent += num;
+    }
+    //screenValue = Number(screen.textContent);
 }
 
 function resetScreen() {
     screen.textContent = "";
-    reset = false;
+    //reset = false;
+}
+
+function reset() {
+    resetScreen();
+    num1 = null;
+    num2 = null;
+    operator = null;
 }
